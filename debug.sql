dae48d770b48882922dcad22f71653107235a8ec
marlsen <marlsenstone@hotmail.com>
2024-09-17 17:44:27 +0800

    init

---
+++ debug.sql
@@ -0,0 +1,35 @@
+select * from mysql.tables;
+
+select * from mysql.schemata;
+
+select * from mysql.catalogs;
+
+select * from mysql.tablespaces;
+
+select * from mysql.indexes;
+select * from mysql.tables;
+select * from mysql.columns;
+
+
+create database `test`;
+use `test`;
+
+
+create table Person
+(
+    id   bigint auto_increment primary key,
+    name varchar(255) not null
+);
+
+
+INSERT INTO Person (name)
+VALUES ('Alice'),
+       ('Bob'),
+       ('Charlie'),
+       ('David'),
+       ('Eve'),
+       ('Frank'),
+       ('Grace'),
+       ('Heidi'),
+       ('Ivan'),
+       ('Judy');
